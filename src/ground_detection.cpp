63d15089e3851fdb6897c9964a152170eb318cec
mlodhi <mulo01@dfki.de>
2025-05-21 09:13:46 +0000

    Adapt cmakelists for header-only

--- src/ground_detection.cpp
+++
@@ -1,6 +0,0 @@
-#include "ground_detection.hpp"
-#include <queue>
-
-namespace pointcloud_obstacle_detection{
-
-} //namespace pointcloud_obstacle_detection
--- src/pointcloud_processor.cpp
+++
@@ -1,7 +0,0 @@
-// PCL lib Functions for processing point clouds 
-
-#include "pointcloud_processor.hpp"
-
-namespace pointcloud_obstacle_detection{
-
-} //namespace pointcloud_obstacle_detection
